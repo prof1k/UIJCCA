6105f3b87a16848e311482c787123f8569d57f22
Иван <@PROF1K>
2018-06-05 20:11:49 +0700

    Операции с ролями

--- Models/ApplicationRoleModels.cs
+++ Models/ApplicationRoleModels.cs
@@ -11,4 +11,15 @@ namespace UIJCCA.web.Models
         public ApplicationRole() { }
         public string Description { get; set; }
     }
+    public class EditRoleModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    public class CreateRoleModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
 }
